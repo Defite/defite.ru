15c837798e5b4bf268a04a5871c364d110ba0f42
Defite <me@defite.ru>
2020-11-19 22:42:58 +0300

    feat(tailwind): move to tailwind 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,93 +1,92 @@
 // tailwind.config.js
 module.exports = {
+  darkMode: "media",
   purge: {
-    enabled: true,
+    mode: "all",
     content: ["./_site/**/*.html"],
   },
   theme: {
     extend: {
-      screens: {
-        dark: { raw: "(prefers-color-scheme: dark)" },
-      },
       colors: {
         dark: "#24283b",
       },
-    },
-    typography: (theme) => ({
-      default: {
-        css: {
-          color: theme("colors.gray.900"),
-          a: {
-            color: theme("colors.blue.700"),
-            "&:hover": {
+
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.gray.900"),
+            a: {
               color: theme("colors.blue.700"),
+              "&:hover": {
+                color: theme("colors.blue.700"),
+                textDecoration: "none",
+              },
+            },
+            "h2 a": {
+              color: theme("colors.gray.900"),
+              textDecoration: "none",
+            },
+            ".tag a": {
               textDecoration: "none",
             },
           },
-          "h2 a": {
-            color: theme("colors.gray.900"),
-            textDecoration: "none",
-          },
-          ".tag a": {
-            textDecoration: "none",
-          },
         },
-      },
 
-      dark: {
-        css: {
-          color: "#7982a9",
-          a: {
-            color: "#9ECE6A",
-            "&:hover": {
+        dark: {
+          css: {
+            color: "#7982a9",
+            a: {
               color: "#9ECE6A",
+              "&:hover": {
+                color: "#9ECE6A",
+              },
+            },
+
+            "h2 a": {
+              color: "#A9B1D6",
+            },
+
+            h1: {
+              color: "#A9B1D6",
+            },
+            h2: {
+              color: "#A9B1D6",
+            },
+            h3: {
+              color: "#A9B1D6",
+            },
+            h4: {
+              color: "#A9B1D6",
+            },
+            h5: {
+              color: "#A9B1D6",
+            },
+            h6: {
+              color: "#A9B1D6",
+            },
+
+            strong: {
+              color: "#A9B1D6",
+            },
+
+            code: {
+              color: "#A9B1D6",
+            },
+
+            figcaption: {
+              color: theme("colors.gray.500"),
+            },
+
+            "::selection": {
+              backgroundColor: "#6f7bb635",
             },
           },
-
-          "h2 a": {
-            color: "#A9B1D6",
-          },
-
-          h1: {
-            color: "#A9B1D6",
-          },
-          h2: {
-            color: "#A9B1D6",
-          },
-          h3: {
-            color: "#A9B1D6",
-          },
-          h4: {
-            color: "#A9B1D6",
-          },
-          h5: {
-            color: "#A9B1D6",
-          },
-          h6: {
-            color: "#A9B1D6",
-          },
-
-          strong: {
-            color: "#A9B1D6",
-          },
-
-          code: {
-            color: "#A9B1D6",
-          },
-
-          figcaption: {
-            color: theme("colors.gray.500"),
-          },
-
-          "::selection": {
-            backgroundColor: "#6f7bb635",
-          },
         },
-      },
-    }),
+      }),
+    },
   },
-  plugins: [
-    require("tailwindcss-dark-mode"),
-    require("@tailwindcss/typography"),
-  ],
+  variants: {
+    typography: ["dark"],
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
